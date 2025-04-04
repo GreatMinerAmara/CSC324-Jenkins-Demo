f1322bde5b4d69c18d072a98cc3ce347d0c3c2c9
GreatMinerAmara <greatmineramara@gmail.com>
2025-04-04 16:22:41 -0500

    Update Utils.java

--- src/Utils.java
+++ src/Utils.java
@@ -2,7 +2,7 @@ public class Utils {
     public static boolean isPositive(int number) {
         return number > 0;
     }
-//Test edit to trigger jenkins
+    
     public static boolean isEven(int number) {
         return number % 2 == 0;
     }
